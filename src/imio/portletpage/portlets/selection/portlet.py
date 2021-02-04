e1bb685e78d34c80cdaa783939dba53e75b81594
Sébastien Verbois <sebastien.verbois@imio.be>
2021-02-04 12:17:34 +0100

    Add Selection Portlet

---
+++ src/imio/portletpage/portlets/selection/portlet.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from importlib.resources import is_resource, path
+from plone import api
+from plone import schema
+from plone.app.z3cform.widget import RichTextFieldWidget
+from plone.app.portlets.portlets import base
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives
+from plone.memoize.instance import memoize
+from plone.portlets.interfaces import IPortletDataProvider
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
+from zope.interface import implementer
+
+from imio.portletpage import _
+
+
+class ISelectionPortlet(IPortletDataProvider):
+
+    header = schema.TextLine(
+        title=_(u"Title"),
+        required=True
+    )
+
+    directives.widget(
+        'content_uids',
+        RelatedItemsFieldWidget,
+        pattern_options={}
+    )
+    content_uids = schema.List(
+        title=_(u"Items"),
+        description=_(u"Select the items of you portlet"),
+        required=True,
+        value_type=schema.Choice(
+            vocabulary='plone.app.vocabularies.Catalog',
+        )
+    )
+
+    # template = schema.Choice(
+    #     title=_(u"Template"),
+    #     required=False,
+    #     default=u"",
+    #     vocabulary='imio.portletpage.PortletSelectionTemplates')
+
+    css_classes = schema.TextLine(
+        title=_(u"CSS Classes"),
+        description=_(u"CSS Classes to personnalize the portlet style"),
+        required=False)
+
+
+@implementer(ISelectionPortlet)
+class Assignment(base.Assignment):
+
+    def __init__(self, header=u"", content_uids=[], css_classes=u""):
+        self.header = header
+        self.content_uids = content_uids
+        self.css_classes = css_classes
+
+    @property
+    def title(self):
+        return self.header
+
+
+class AddForm(base.AddForm):
+    schema = ISelectionPortlet
+    label = _(u'Add Selection Portlet')
+
+    def create(self, data):
+        return Assignment(**data)
+
+
+class EditForm(base.EditForm):
+    schema = ISelectionPortlet
+    label = _(u'Edit Selection Portlet')
+
+
+class Renderer(base.Renderer):
+    render = ViewPageTemplateFile("portlet.pt")
+
+    @property
+    @memoize
+    def contents(self):
+        return [api.content.get(UID=uid) for uid in self.data.content_uids]
+
+    @property
+    @memoize
+    def items(self):
+        infos = []
+        for obj in self.contents:
+            infos.append({
+                'title': obj.title,
+                'description': obj.description if hasattr(obj, "description") else None,
+                'url': obj.remoteUrl  if hasattr(obj, "remoteUrl") else obj.absolute_url(),  # Use remoteUrl for Link content type
+                'image_url': obj.absolute_url() + "/@@images/image/preview" if (hasattr(obj, "image") and obj.image) else None,
+            })
+        return infos
